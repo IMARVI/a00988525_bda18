dd771283f66f9d771170f37bc326a041fc18f48b
marvi <isaiasmarvi@gmail.com>
2018-02-19 11:19:31 -0600

    Cursores

--- Actividades/A9.Cursores/A00988525_A9_1_Cursores.sql
+++ Actividades/A9.Cursores/A00988525_A9_1_Cursores.sql
@@ -21,3 +21,54 @@ begin
 		select ids;
         close cursor1;
 end //
+
+-- ------------------------------------------------- Tarea
+
+use sakila;
+
+drop procedure categoria_peli;
+
+-- select * from film_category;
+
+delimiter //
+
+create procedure categoria_peli()
+
+begin
+    declare done int default false;
+	declare nuevoNombre varchar(60);
+    
+    declare peli varchar(30);
+    declare cat varchar(30);
+    
+    declare c,p int;
+    declare p1 cursor for select film_id from film_category;
+	declare c1 cursor for select category_id from film_category;
+	
+    declare continue handler for not found set done = true;
+			
+	open p1;
+    open c1;
+		
+	read_loop: loop
+		
+        fetch p1 into p;
+        fetch c1 into c;
+        set peli = (select title from film where film_id = p);
+        set cat = (select name from category where category_id = c);
+		
+        if done then
+			leave read_loop;
+		end if;
+        
+        if film.film_id = p then
+			set nuevoNombre = concat(cat,'_',peli);
+		end if;
+        
+	end loop;
+    
+    close p1; 
+    close c1;
+end //
+
+call categoria_peli;
